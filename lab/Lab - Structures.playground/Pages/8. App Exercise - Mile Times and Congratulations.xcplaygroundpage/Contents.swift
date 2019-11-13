980b0cd46144d0f1d722f4b59b4751687604120f
Liam Davidson <ldavidson@lcs.on.ca>
2019-11-13 13:37:05 -0500

    Finished 2.3.8

--- lab/Lab - Structures.playground/Pages/8. App Exercise - Mile Times and Congratulations.xcplaygroundpage/Contents.swift
+++ lab/Lab - Structures.playground/Pages/8. App Exercise - Mile Times and Congratulations.xcplaygroundpage/Contents.swift
@@ -25,11 +25,20 @@ print(myRun.averageMileTime)
  In the `Steps` struct below, add a `willSet` to the `steps` property that will check if the new value is equal to `goal`, and if it is, prints a congratulatory message. Create an instance of `Steps` where `steps` is 9999 and `goal` is 10000, then call `takeStep()` and see if your message is printed to the console.
  */
 struct Steps {
-    var steps: Int
+    var steps: Int {
+        willSet{
+            if newValue == goal {
+                print("Well done, you've reached your daily goal!")
+            }
+        }
+    }
     var goal: Int
     
     mutating func takeStep() {
         steps += 1
     }
 }
+
+var todaySteps = Steps(steps: 9999, goal: 10_000)
+todaySteps.takeStep()
 //: [Previous](@previous)  |  page 8 of 10  |  [Next: Exercise - Type Properties and Methods](@next)
