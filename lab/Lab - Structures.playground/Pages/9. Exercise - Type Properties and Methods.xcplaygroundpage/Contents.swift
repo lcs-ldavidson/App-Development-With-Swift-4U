3f78ecad881bd5e20ad3ed30319b02e0d5da044c
Liam Davidson <ldavidson@lcs.on.ca>
2019-11-13 14:07:04 -0500

    Finished 2.3.9

--- lab/Lab - Structures.playground/Pages/9. Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
+++ lab/Lab - Structures.playground/Pages/9. Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
@@ -7,12 +7,24 @@ struct User {
     var userName: String
     var email: String
     var age: Int
+    
+    static var currentUser = User(userName: "Liam Davidson", email: "something@email.org", age: 17)
+    
+    static func logIn(user: User) {
+        self.currentUser = user
+        print("\(currentUser.userName) has successfully logged in")
+    }
 }
+
+print(User.currentUser.age)
+print(User.currentUser.email)
+print(User.currentUser.userName)
 /*:
  There are other properties and actions associated with a `User` struct that might be good candidates for a type property or method. One might be a method for logging in. Go back and create a type method called `logIn(user:)` where `user` is of type `User`. In the body of the method, assign the passed in user to the `currentUser` property, and print out a statement using the user's userName saying that the user has logged in.
  
  Below, call the `logIn(user:)` method and pass in a different `User` instance than what you assigned to currentUser above. Observe the printout in the console.
  */
-
+var me = User(userName: "xXDarkLordWeedMaster69Xx", email: "liam@ginger.ca", age: 76)
+User.logIn(user: me)
 
 //: [Previous](@previous)  |  page 9 of 10  |  [Next: App Exercise - Type Properties and Methods](@next)
